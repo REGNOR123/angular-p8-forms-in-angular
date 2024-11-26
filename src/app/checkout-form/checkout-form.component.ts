b7f42e8dfee6dc661a0c7a6d7bf3ebf4862492bd
Amit Kumar <amit1711030007@gmail.com>
2024-11-27 00:11:53 +0530

    reset the form fields value

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -3,24 +3,44 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms'; // STEP-3 
 @Component({
   selector: 'app-checkout-form',
   templateUrl: './checkout-form.component.html',
-  styleUrls: ['./checkout-form.component.css']
+  styleUrls: ['./checkout-form.component.css'],
 })
 export class CheckoutFormComponent implements OnInit {
   checkoutForm: FormGroup; // providing the formgroup name
   constructor(private formBuilder: FormBuilder) {
-    this.checkoutForm = formBuilder.group({      //building the form using formbuilder
-      emailFiled: new FormControl(),   //in the formbuilder - we are creating a group of form elements
+    //FormBuilder help us to build the form
+    this.checkoutForm = formBuilder.group({
+      //building the form using formbuilder
+      emailFiled: new FormControl(), //in the formbuilder - we are creating a group of form elements
       passwordField: new FormControl(), //these should be exactly same as your form
       checkBoxField: new FormControl(),
     });
-  } //FormBuilder help us to build the form
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.checkoutForm.setValue({        // set value for all the fields in one go
+      emailFiled: 'akr009020@gmail.com',
+      passwordField: '12345678',
+      checkBoxField: true,
+    });
 
-  handleSubmit(){
-    console.log(this.checkoutForm);                      //STEP-4 : getting the input fields value
+    // this.checkoutForm.patchValue({        // set value for individual fields
+    //   emailFiled: 'akr009020@gmail.com',
+    //   checkBoxField: true,
+    // });
+
+    //this.checkoutForm.reset();    // reset all the values
+  }
+
+  handleSubmit() {
+    console.log(this.checkoutForm); // getting fields value in console
     console.log(this.checkoutForm.value.emailFiled);
     console.log(this.checkoutForm.value.passwordField);
     console.log(this.checkoutForm.value.checkBoxField);
+
+    this.checkoutForm.reset();
+  }
+  handleCancel(){
+    this.checkoutForm.reset();
   }
 }
