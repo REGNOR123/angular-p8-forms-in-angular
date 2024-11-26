6fb9ac71edce448f84f106fe4d8a7419840dc613
Amit Kumar <amit1711030007@gmail.com>
2024-11-26 23:54:44 +0530

    get and set the form fields values

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -12,7 +12,7 @@ import {
 })
 export class CheckoutFormComponent implements OnInit {
   checkoutForm: FormGroup; // providing the formgroup name
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {  //FormBuilder help us to build the form
     this.checkoutForm = formBuilder.group({
       //building the form using formbuilder
       emailFiled: new FormControl(), //in the formbuilder - we are creating a group of form elements
@@ -29,12 +29,23 @@ export class CheckoutFormComponent implements OnInit {
         ],
       ],
     });
-  } //FormBuilder help us to build the form
+  }  
 
-  ngOnInit(): void {}
+  ngOnInit(): void { 
+    // this.checkoutForm.setValue({        // setting up the form values all the fields at once
+    //   emailFiled: 'akr009020@gmail.com',
+    //   passwordField: '123456@##',
+    //   checkBoxField: true
+    // })
+
+     this.checkoutForm.patchValue({        // setting up the form values for individual fields
+      emailFiled: 'akr009020@gmail.com',
+      checkBoxField: true
+    })
+  }
 
   handleSubmit() {
-    console.log(this.checkoutForm); //STEP-4 : getting the input fields value
+    console.log(this.checkoutForm); //: getting the input fields value
     console.log(this.checkoutForm.value.emailFiled);
     console.log(this.checkoutForm.value.passwordField);
     console.log(this.checkoutForm.value.checkBoxField);
