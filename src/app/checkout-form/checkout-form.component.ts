5e6608fdbb0c57a122b32be4a93b60e9076e6a0e
Amit Kumar <amit1711030007@gmail.com>
2024-11-26 23:00:57 +0530

    Validations in reactive forms

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms'; // STEP-3 : Import all the methods of forms
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms'; // STEP-3 : Import all the methods of forms
 @Component({
   selector: 'app-checkout-form',
   templateUrl: './checkout-form.component.html',
-  styleUrls: ['./checkout-form.component.css']
+  styleUrls: ['./checkout-form.component.css'],
 })
 export class CheckoutFormComponent implements OnInit {
   checkoutForm: FormGroup; // providing the formgroup name
   constructor(private formBuilder: FormBuilder) {
-    this.checkoutForm = formBuilder.group({      //building the form using formbuilder
-      emailFiled: new FormControl(),   //in the formbuilder - we are creating a group of form elements
+    this.checkoutForm = formBuilder.group({
+      //building the form using formbuilder
+      emailFiled: new FormControl(), //in the formbuilder - we are creating a group of form elements
       passwordField: new FormControl(), //these should be exactly same as your form
       checkBoxField: new FormControl(),
+    });
+    this.checkoutForm = formBuilder.group({
+      emailFiled: ['', [Validators.required, Validators.email]], // implementing the validqation in the required fields
+      passwordField: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+        ],
+      ],
     });
   } //FormBuilder help us to build the form
 
   ngOnInit(): void {}
 
-  handleSubmit(){
-    console.log(this.checkoutForm);                      //STEP-4 : getting the input fields value
+  handleSubmit() {
+    console.log(this.checkoutForm); //STEP-4 : getting the input fields value
     console.log(this.checkoutForm.value.emailFiled);
     console.log(this.checkoutForm.value.passwordField);
     console.log(this.checkoutForm.value.checkBoxField);
